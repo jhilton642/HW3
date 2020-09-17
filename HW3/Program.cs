b61af915cc580dd27b96ac56569bbdb717b97ec7
jhilton642 <joshua.hilton@att.net>
2020-09-17 10:50:47 -0500

    Add project files.

---
+++ HW3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = 0;
+            int secondNum = 1;
+            Console.WriteLine("Enter how many Fibonacci numbers do you want to see? ");
+            string howMany = Console.ReadLine();
+            int howManyNums = Convert.ToInt32(howMany);
+            Console.WriteLine(firstNum);
+            for (int i = 2; i <= howManyNums; i++)
+            {
+                int temp = firstNum;
+                firstNum = firstNum + secondNum;
+                secondNum = temp;
+                Console.WriteLine(firstNum);
+            }
+        }
+    }
+}
